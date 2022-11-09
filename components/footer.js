706e12bca3a6b0223fcfc4ae4b88aac51a9ed5a4
eozefow <eozefow@gmail.com>
2022-11-10 02:24:32 +1100

    add toggle button

--- components/footer.js
+++ components/footer.js
@@ -2,7 +2,7 @@ export default function Footer() {
   return (
     <footer className="py-6 text-center text-gray-500 text-sm">
       <span className="text-gray-900 font-bold text-lg mr-2">Joseph</span>{" "}
-      &copy; {new Date().getFullYear} All Rights Reversed
+      &copy; {new Date().getFullYear()} All Rights Reversed
     </footer>
   );
 }
--- components/header.js
+++ components/header.js
@@ -1,10 +1,31 @@
 import Link from "next/link";
+import Button from "../components/button";
 
 export default function Header() {
   const navigations = [
     { label: "Home", path: "/" },
     { label: "About", path: "./about" },
   ];
+
+  const renderThemeChanger = () => {
+    return (
+      <Button className="">
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          viewBox="0 0 24 24"
+          fill="currentColor"
+          className="w-6 h-6"
+        >
+          <path
+            fillRule="evenodd"
+            d="M9.528 1.718a.75.75 0 01.162.819A8.97 8.97 0 009 6a9 9 0 009 9 8.97 8.97 0 003.463-.69.75.75 0 01.981.98 10.503 10.503 0 01-9.694 6.46c-5.799 0-10.5-4.701-10.5-10.5 0-4.368 2.667-8.112 6.46-9.694a.75.75 0 01.818.162z"
+            clipRule="evenodd"
+          />
+        </svg>
+      </Button>
+    );
+  };
+
   return (
     <header className="h-16 flex items-center justify-between">
       <div className="flex gap-4 ">
@@ -18,7 +39,7 @@ export default function Header() {
           </Link>
         ))}
       </div>
-      <button>Icon</button>
+      {renderThemeChanger()}
     </header>
   );
 }
